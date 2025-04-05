3e6218d42fbbccdfcf25be4ea379462b604c0dd9
alvaro-r-g <alvaro.r.g@um.es>
2025-04-05 18:28:57 +0200

    Add UnmatchedBegin and CallInTransaction

--- clang/lib/StaticAnalyzer/Checkers/HTMChecker.cpp
+++ clang/lib/StaticAnalyzer/Checkers/HTMChecker.cpp
@@ -10,19 +10,23 @@ using namespace clang;
 using namespace ento;
 
 namespace {
-class HTMChecker : public Checker<check::PreCall> {
+class HTMChecker : public Checker<check::PreCall, check::EndFunction> {
   const CallDescription BeginFn{CDM::SimpleFunc, {"nstm_begin"}, 0};
   const CallDescription EndFn{CDM::SimpleFunc, {"nstm_end"}, 0};
 
   const BugType DoubleBeginBugType{this, "Double begin"};
   const BugType UnmatchedEnd{this, "Unmatched end"};
+  const BugType UnmatchedBegin{this, "Unmatched begin"};
+  const BugType CallInTransaction{this, "Call in transaction"};
 
   void reportDoubleBegin(const CallEvent &Call, CheckerContext &C) const;
-
   void reportUnmatchedEnd(const CallEvent &Call, CheckerContext &C) const;
+  void reportUnmatchedBegin(const Stmt *S, CheckerContext &C) const;
+  void reportCallInTransaction(const CallEvent &Call, CheckerContext &C) const;
 
 public:
   void checkPreCall(const CallEvent &Call, CheckerContext &C) const;
+  void checkEndFunction(const ReturnStmt *RS, CheckerContext &C) const;
 };
 
 } // end anonymous namespace
@@ -34,22 +38,48 @@ void HTMChecker::checkPreCall(const CallEvent &Call, CheckerContext &C) const {
   const bool inTransaction = State->get<InTransactionState>();
   const bool isBegin = BeginFn.matches(Call);
   const bool isEnd = EndFn.matches(Call);
+  // const bool isInlined = Call.getDecl()->getAsFunction()->isInlined();
 
   assert(!(isBegin && isEnd));
 
-  if (isBegin && inTransaction) {
-    reportDoubleBegin(Call, C);
-    return;
-  }
+  if (isBegin || isEnd) {
+    if (isBegin && inTransaction) {
+      reportDoubleBegin(Call, C);
+      return;
+    }
 
-  if (isEnd && !inTransaction) {
-    reportUnmatchedEnd(Call, C);
-    return;
-  }
+    if (isEnd && !inTransaction) {
+      reportUnmatchedEnd(Call, C);
+      return;
+    }
 
-  State = State->set<InTransactionState>(isBegin);
-  C.addTransition(State);
+    State = State->set<InTransactionState>(isBegin);
+    C.addTransition(State);
+  } else if (inTransaction) {
+    reportCallInTransaction(Call, C);
+  }
 }
+
+void HTMChecker::checkEndFunction(const ReturnStmt *RS,
+                                  CheckerContext &C) const {
+  ProgramStateRef State = C.getState();
+
+  bool isBeginOrEnd = false;
+
+  const Stmt *CallSite = C.getLocationContext()->getStackFrame()->getCallSite();
+  const CallExpr *Call = dyn_cast_or_null<CallExpr>(CallSite);
+  if (Call &&
+      (BeginFn.matchesAsWritten(*Call) || EndFn.matchesAsWritten(*Call))) {
+    isBeginOrEnd = true;
+  }
+
+  const bool inTransaction = State->get<InTransactionState>();
+  if (!isBeginOrEnd && inTransaction) {
+    reportUnmatchedBegin(RS, C);
+  }
+}
+
+// TODO: Better warning notes
 
 void HTMChecker::reportDoubleBegin(const CallEvent &Call,
                                    CheckerContext &C) const {
@@ -59,7 +89,7 @@ void HTMChecker::reportDoubleBegin(const CallEvent &Call,
 
   auto R = std::make_unique<PathSensitiveBugReport>(
       DoubleBeginBugType,
-      "Trying to start a transaction while inside a transaction (target "
+      "Trying to start a transaction while in transactional state (target "
       "implementation does not support nesting)",
       ErrNode);
   R->addRange(Call.getSourceRange());
@@ -67,14 +97,41 @@ void HTMChecker::reportDoubleBegin(const CallEvent &Call,
 }
 
 void HTMChecker::reportUnmatchedEnd(const CallEvent &Call,
-                                 CheckerContext &C) const {
+                                    CheckerContext &C) const {
   ExplodedNode *ErrNode = C.generateErrorNode();
   if (!ErrNode)
     return;
 
   auto R = std::make_unique<PathSensitiveBugReport>(
-      UnmatchedEnd,
-      "Trying to end a transaction while outside a transaction", ErrNode);
+      UnmatchedEnd, "Trying to end a transaction while outside a transaction",
+      ErrNode);
+  R->addRange(Call.getSourceRange());
+  C.emitReport(std::move(R));
+}
+
+void HTMChecker::reportUnmatchedBegin(const Stmt *S, CheckerContext &C) const {
+  ExplodedNode *ErrNode = C.generateErrorNode();
+  if (!ErrNode)
+    return;
+
+  auto R = std::make_unique<PathSensitiveBugReport>(
+      UnmatchedBegin,
+      "Trying to exit the current function while in transactional state",
+      ErrNode);
+  if (S)
+    R->addRange(S->getSourceRange());
+  C.emitReport(std::move(R));
+}
+
+void HTMChecker::reportCallInTransaction(const CallEvent &Call,
+                                         CheckerContext &C) const {
+  ExplodedNode *ErrNode = C.generateErrorNode();
+  if (!ErrNode)
+    return;
+
+  auto R = std::make_unique<PathSensitiveBugReport>(
+      CallInTransaction,
+      "Trying to call a function while in transactional state", ErrNode);
   R->addRange(Call.getSourceRange());
   C.emitReport(std::move(R));
 }
@@ -83,5 +140,5 @@ void ento::registerHTMChecker(CheckerManager &mgr) {
   mgr.registerChecker<HTMChecker>();
 }
 
-// This checker should be enabled regardless of how language options are set.
+// This checker should be enabled regardless of how language options are set
 bool ento::shouldRegisterHTMChecker(const CheckerManager &mgr) { return true; }
