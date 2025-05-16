be54758f764f46a6ecf737293b764b43dc1aeb46
alvaro-r-g <alvaro.r.g@um.es>
2025-05-16 18:27:50 +0200

    Count statements

--- clang/lib/StaticAnalyzer/Checkers/HTMChecker.cpp
+++ clang/lib/StaticAnalyzer/Checkers/HTMChecker.cpp
@@ -10,54 +10,94 @@ using namespace clang;
 using namespace ento;
 
 namespace {
-class HTMChecker : public Checker<check::PreCall, check::EndFunction> {
+class HTMChecker : public Checker<check::PreCall, check::EndFunction,
+                                  check::PostStmt<Stmt>> {
   const CallDescription BeginFn{CDM::SimpleFunc, {"nstm_begin"}, 0};
   const CallDescription EndFn{CDM::SimpleFunc, {"nstm_end"}, 0};
 
-  const BugType DoubleBeginBugType{this, "Double begin"};
+  const BugType DoubleBegin{this, "Double begin"};
   const BugType UnmatchedEnd{this, "Unmatched end"};
   const BugType UnmatchedBegin{this, "Unmatched begin"};
   const BugType CallInTransaction{this, "Call in transaction"};
+  const BugType MaxStatementCountExceeded{this, "Statement count exceeded"};
+
+  static constexpr unsigned MAX_STMTS = 50;
 
   void reportDoubleBegin(const CallEvent &Call, CheckerContext &C) const;
   void reportUnmatchedEnd(const CallEvent &Call, CheckerContext &C) const;
   void reportUnmatchedBegin(const Stmt *S, CheckerContext &C) const;
   void reportCallInTransaction(const CallEvent &Call, CheckerContext &C) const;
+  void reportInstructionCountExceeded(const CallEvent &Call, CheckerContext &C,
+                                      unsigned Count) const;
 
 public:
   void checkPreCall(const CallEvent &Call, CheckerContext &C) const;
   void checkEndFunction(const ReturnStmt *RS, CheckerContext &C) const;
+  void checkPostStmt(const Stmt *S, CheckerContext &C) const;
 };
 
 } // end anonymous namespace
 
 REGISTER_TRAIT_WITH_PROGRAMSTATE(InTransactionState, bool);
+REGISTER_TRAIT_WITH_PROGRAMSTATE(InstructionCountState, unsigned);
 
 void HTMChecker::checkPreCall(const CallEvent &Call, CheckerContext &C) const {
   ProgramStateRef State = C.getState();
   const bool inTransaction = State->get<InTransactionState>();
+  const unsigned instructionCount = State->get<InstructionCountState>();
+
   const bool isBegin = BeginFn.matches(Call);
   const bool isEnd = EndFn.matches(Call);
   // const bool isInlined = Call.getDecl()->getAsFunction()->isInlined();
 
   assert(!(isBegin && isEnd));
 
-  if (isBegin || isEnd) {
-    if (isBegin && inTransaction) {
+  if (isBegin) {
+    if (inTransaction) {
       reportDoubleBegin(Call, C);
       return;
     }
 
-    if (isEnd && !inTransaction) {
+    State = State->set<InTransactionState>(true);
+    State = State->set<InstructionCountState>(0);
+    C.addTransition(State);
+  } else if (isEnd) {
+    if (!inTransaction) {
       reportUnmatchedEnd(Call, C);
       return;
     }
 
-    State = State->set<InTransactionState>(isBegin);
+    State = State->set<InTransactionState>(false);
+    State = State->set<InstructionCountState>(0);
     C.addTransition(State);
-  } else if (inTransaction) {
-    reportCallInTransaction(Call, C);
+
+    if (instructionCount > MAX_STMTS) {
+      reportInstructionCountExceeded(Call, C, instructionCount);
+    }
+  } else {
+    if (inTransaction) {
+      reportCallInTransaction(Call, C);
+    }
   }
+}
+
+void HTMChecker::checkPostStmt(const Stmt *S, CheckerContext &C) const {
+  ProgramStateRef State = C.getState();
+  if (!State->get<InTransactionState>()) {
+    return;
+  }
+
+  // Skip begin and end
+  if (const CallExpr *CE = dyn_cast<CallExpr>(S)) {
+    if (BeginFn.matchesAsWritten(*CE) || EndFn.matchesAsWritten(*CE)) {
+      return;
+    }
+  }
+
+  unsigned count = State->get<InstructionCountState>();
+  count++;
+  State = State->set<InstructionCountState>(count);
+  C.addTransition(State);
 }
 
 void HTMChecker::checkEndFunction(const ReturnStmt *RS,
@@ -88,7 +128,7 @@ void HTMChecker::reportDoubleBegin(const CallEvent &Call,
     return;
 
   auto R = std::make_unique<PathSensitiveBugReport>(
-      DoubleBeginBugType,
+      DoubleBegin,
       "Trying to start a transaction while in transactional state (target "
       "implementation does not support nesting)",
       ErrNode);
@@ -125,7 +165,7 @@ void HTMChecker::reportUnmatchedBegin(const Stmt *S, CheckerContext &C) const {
 
 void HTMChecker::reportCallInTransaction(const CallEvent &Call,
                                          CheckerContext &C) const {
-  ExplodedNode *ErrNode = C.generateNonFatalErrorNode();
+  ExplodedNode *ErrNode = C.generateErrorNode();
   if (!ErrNode)
     return;
 
@@ -136,9 +176,23 @@ void HTMChecker::reportCallInTransaction(const CallEvent &Call,
   C.emitReport(std::move(R));
 }
 
+void HTMChecker::reportInstructionCountExceeded(const CallEvent &Call,
+                                                CheckerContext &C,
+                                                unsigned Count) const {
+  ExplodedNode *ErrNode = C.generateErrorNode();
+  if (!ErrNode)
+    return;
+
+  std::string Msg = "Transaction contains " + std::to_string(Count) +
+                    " instructions (max is " + std::to_string(MAX_STMTS) + ")";
+  auto R = std::make_unique<PathSensitiveBugReport>(MaxStatementCountExceeded,
+                                                    Msg, ErrNode);
+  R->addRange(Call.getSourceRange());
+  C.emitReport(std::move(R));
+}
+
 void ento::registerHTMChecker(CheckerManager &mgr) {
   mgr.registerChecker<HTMChecker>();
 }
 
-// This checker should be enabled regardless of how language options are set
 bool ento::shouldRegisterHTMChecker(const CheckerManager &mgr) { return true; }
